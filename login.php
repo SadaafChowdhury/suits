9ae2cb459fdccc2e13f028273d0f29c5a190179e
Sadaaf Chowdhury <sadaafchowdhury@northsouth.edu>
2020-08-18 00:15:44 +0600

    Login

--- login.php
+++ login.php
@@ -34,15 +34,39 @@
 <div class="registrationDiv">
 <h1>Login</h1>
 
-<form action="signup.php" method="POST">
+<form method="POST">
 	<label><b>Email:</b></label><br>
 	<input class="fields" type="email" placeholder="Enter Email" name="email" id="email" required><br><br>
 	<label><b>Password:</b></label><br>
-	<input class="fields" type="text" placeholder="Enter Password" name="password" id="password" required><br><br><br><br>
+	<input class="fields" type="password" placeholder="Enter Password" name="password" id="password" required><br><br><br><br>
 	
-	<input class="button1" type="submit" name="create" value="Login"></input>
+	<input class="button1" type="submit" name="submit" value="Login"></input>
 </form>
 </div>
-
+<?php
+	$dbServername = "localhost";
+	$dbUsername="root";
+	$dbPassword ="";
+	$dbName = "suits";
+	
+	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+	
+	if(isset($_POST['submit'])){
+		$email = $_POST["email"];
+		$password = $_POST["password"];
+	}
+	$select = mysql_query($conn, "select * from 'users'"); //query to select all rows from users table
+	while($result=mysql_fetch_arrayi($select)){
+		$mail = $result['email'];
+		$passwords = $result['password'];
+		
+		if($passwords!=$password || $mail!=$email){
+			echo "Try again, email or password is incorrect.";
+		}
+		else{
+			echo "Login Successful";
+		}
+	}
+?>
 </body>
 </html>
